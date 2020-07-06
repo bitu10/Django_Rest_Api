570f130a7abc656b0a0a35700c9645c691ee289d
bitu10 <sachin.bitupon@gmail.com>
2020-07-07 02:51:14 +0530

    1st commit

--- full_throtle_labs/urls.py
+++ full_throtle_labs/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('basic/', include('api_basic.urls'))
+    path('', include('api_basic.urls'))
 
 ]
